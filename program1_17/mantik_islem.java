09beff01424b0cf07f376a73b28cfe2c798f7483
Ertuğrul Tekin <ertugrul_tekin.23@hotmail.com>
2020-04-11 02:17:30 +0300

    Program1_17 Mantık operatörleri ve işlevleri anlatıldı. Program1_18 Bit opretörleri ve işlevleri anlatıldı örnek verildi.

---
+++ program1_17/mantik_islem.java
@@ -0,0 +1,106 @@
+
+import java.io.*;
+
+
+
+class Mantik_islem
+{
+	
+	public static void main(String[] args)
+	{
+	System.out.println("Üç değişken tanımlayıp bunları kıyaslayacağız");
+	
+	int a = 4;
+	int b = 8;
+	int c = 8;
+	
+	
+	System.out.println(" a değişkeni 4");
+	System.out.println(" b değişkeni 8");
+	System.out.println(" c değişkeni 8");
+	
+	System.out.println("----------------");
+	System.out.println("Birde boolean değişkeni tanıplayıp nasıl şekil aldığını inceleyelim.");
+	
+	boolean TorF;
+	
+	
+	System.out.println("----------------");
+	TorF=a<b;
+	System.out.println("a<b= " + TorF);
+	System.out.println("----------------");
+	System.out.println("* \n * \n * \n *");
+	
+	
+	System.out.println("----------------");
+	TorF=a>b;
+	System.out.println("a>b= " + TorF);
+	System.out.println("----------------");
+	System.out.println("* \n * \n * \n *");
+	
+	
+	System.out.println("----------------");
+	TorF=a<=b;
+	System.out.println("a<=b = " + TorF);
+	System.out.println("----------------");
+	System.out.println("* \n * \n * \n *");
+	
+	
+	System.out.println("----------------");
+	TorF=a>=b;
+	System.out.println("a>=b= " + TorF);
+	System.out.println("----------------");
+	System.out.println("* \n * \n * \n *");
+	
+	
+	
+	System.out.println("----------------");
+	TorF=b<c;
+	System.out.println("b<c  " + TorF);
+	System.out.println("----------------");
+	System.out.println("* \n * \n * \n *");
+	
+	
+	System.out.println("----------------");
+	TorF=b>c;
+	System.out.println("b>c  " + TorF);
+	System.out.println("----------------");
+	System.out.println("* \n * \n * \n *");
+	
+	
+	System.out.println("----------------");
+	TorF=b<=c;
+	System.out.println("b<=b  " + TorF);
+	System.out.println("----------------");
+	System.out.println("* \n * \n * \n *");
+	
+	
+	System.out.println("----------------");
+	TorF=b>c;
+	System.out.println("a>=b  " + TorF);
+	System.out.println("----------------");
+	System.out.println("* \n * \n * \n *");
+	
+	
+	System.out.println("----------------");
+	TorF=b==c;
+	System.out.println("c==b  " + TorF);
+	System.out.println("----------------");
+	System.out.println("* \n * \n * \n *");
+	
+	
+	System.out.println("----------------");
+	TorF=a==c;
+	System.out.println("a==b  " + TorF);
+	System.out.println("----------------");
+	System.out.println("* \n * \n * \n *");	
+	
+	
+	
+	
+	}
+	
+
+
+}
+
